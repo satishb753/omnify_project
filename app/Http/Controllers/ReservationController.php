b94b2bb93e630d89b6bff5a44065344c1637345e
satishb753 <satishb753@gmail.com>
2022-02-03 16:38:42 +0530

    query fix 1.0

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -70,7 +70,7 @@ class ReservationController extends Controller
                 break;
             case 'week':
                 $previousReservationsOfSameWeek = \DB::table('reservations')
-                                    ->whereIn('user_id',$user_ids[0])
+                                    ->whereIn('user_id',$user_ids)
                                     ->where('reservation_timestamp_utc','<=',$incoming_datetime)
                                     ->where('reservation_timestamp_utc','>',$incoming_datetime - 7*24*60*60)
                                     ->orderBy('reservation_timestamp_utc','desc');
@@ -98,7 +98,7 @@ class ReservationController extends Controller
                 break;
             case 'month':
                 $previousReservationsOfSameMonth = \DB::table('reservations')
-                                    ->whereIn('user_id',$user_ids[0])
+                                    ->whereIn('user_id',$user_ids)
                                     ->where('reservation_timestamp_utc','<=',$incoming_datetime)
                                     ->where('reservation_timestamp_utc','>',$incoming_datetime - 30*24*60*60)
                                     ->orderBy('reservation_timestamp_utc','desc');
